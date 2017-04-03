c9d838bf947cf65394b99ae05b98078afa5ccca4
Dan Cook <leinad13@gmail.com>
2017-04-03 13:08:08 +0100

    Added source files

---
+++ GraphiteClientGenerator/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace GraphiteClientGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpMain = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHostname = new System.Windows.Forms.TextBox();
+            this.trvPerfs = new System.Windows.Forms.TreeView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.grpConfig = new System.Windows.Forms.GroupBox();
+            this.grpMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpMain
+            // 
+            this.grpMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grpMain.Controls.Add(this.splitContainer1);
+            this.grpMain.Controls.Add(this.button1);
+            this.grpMain.Controls.Add(this.label1);
+            this.grpMain.Controls.Add(this.txtHostname);
+            this.grpMain.Location = new System.Drawing.Point(12, 12);
+            this.grpMain.Name = "grpMain";
+            this.grpMain.Size = new System.Drawing.Size(703, 596);
+            this.grpMain.TabIndex = 0;
+            this.grpMain.TabStop = false;
+            this.grpMain.Text = "MainConfig";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Hostname :";
+            // 
+            // txtHostname
+            // 
+            this.txtHostname.Location = new System.Drawing.Point(67, 19);
+            this.txtHostname.Name = "txtHostname";
+            this.txtHostname.Size = new System.Drawing.Size(157, 20);
+            this.txtHostname.TabIndex = 1;
+            this.txtHostname.Text = "localhost";
+            // 
+            // trvPerfs
+            // 
+            this.trvPerfs.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.trvPerfs.CheckBoxes = true;
+            this.trvPerfs.Location = new System.Drawing.Point(3, 3);
+            this.trvPerfs.Name = "trvPerfs";
+            this.trvPerfs.Size = new System.Drawing.Size(438, 538);
+            this.trvPerfs.TabIndex = 0;
+            this.trvPerfs.AfterCheck += new System.Windows.Forms.TreeViewEventHandler(this.trvPerfs_AfterCheck);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(230, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(6, 46);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.trvPerfs);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.grpConfig);
+            this.splitContainer1.Size = new System.Drawing.Size(691, 544);
+            this.splitContainer1.SplitterDistance = 444;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // grpConfig
+            // 
+            this.grpConfig.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grpConfig.Location = new System.Drawing.Point(3, 3);
+            this.grpConfig.Name = "grpConfig";
+            this.grpConfig.Size = new System.Drawing.Size(237, 538);
+            this.grpConfig.TabIndex = 0;
+            this.grpConfig.TabStop = false;
+            this.grpConfig.Text = "Config";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(727, 620);
+            this.Controls.Add(this.grpMain);
+            this.Name = "Form1";
+            this.Text = "Graphite Config Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grpMain.ResumeLayout(false);
+            this.grpMain.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpMain;
+        private System.Windows.Forms.TreeView trvPerfs;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHostname;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox grpConfig;
+    }
+}
+
---
+++ GraphiteClientGenerator/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
+
+namespace GraphiteClientGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {           
+            
+
+        }
+
+        private void LoadPerformanceCounters(string hostname)
+        {
+            // Check Host is alive / online
+            try
+            {
+                // Get IP Address of System
+                System.Net.IPAddress address = System.Net.Dns.GetHostEntry(hostname).AddressList.First();
+
+                // Ping Object
+                Ping ping = new Ping();
+                // Ping buffer - 32 byte buffer (create empty)
+                byte[] buffer = new byte[32];
+                // Ping Options
+                PingOptions pingOptions = new PingOptions(128, true);
+
+                // Ping Host 4 times
+                for (int i = 0; i < 4; i++)
+                {
+                    // PingReply Object
+                    PingReply pingReply = ping.Send(address, 1000, buffer, pingOptions);
+                    //make sure we dont have a null reply
+                    if (!(pingReply == null))
+                    {
+                        switch (pingReply.Status)
+                        {
+                            case IPStatus.Success:
+                                break;
+                            case IPStatus.TimedOut:
+                                throw new PingException("Timed Out");
+                            default:
+                                throw new PingException("Ping Failed");
+                        }
+                    }
+                }        
+            }
+            catch (PingException ex)
+            {
+                MessageBox.Show(ex.Message, "Problem Connecting to "+hostname,MessageBoxButtons.OK);
+                return;
+            }
+
+            // Get Hostname to use for root tree node
+            TreeNode rootnode = new TreeNode(hostname);
+
+            // Get Performance Counter Categories
+            List<PerformanceCounterCategory> categories = PerformanceCounterCategory.GetCategories(hostname).OrderBy(category => category.CategoryName).ToList();
+
+            foreach (PerformanceCounterCategory cat in categories)
+            {
+                TreeNode node = new TreeNode(cat.CategoryName);
+                try
+                {
+                    // Get Perf Counter Instance
+                    string[] instances = cat.GetInstanceNames();
+                    foreach (string instance in instances)
+                    {
+                        TreeNode n = new TreeNode(instance);
+
+                        // Get Perf Counter Name
+                        PerformanceCounter[] perfs = cat.GetCounters(instance);
+                        foreach (PerformanceCounter perf in perfs)
+                        {
+                            TreeNode counternode = new TreeNode(perf.CounterName);
+                            n.Nodes.Add(counternode);
+                        }
+
+                        node.Nodes.Add(n);
+                    }
+                } catch (Exception ex)
+                {
+                    // Some Performance Counters throw an error when trying to enumerate instances
+                }
+                PerformanceCounter[] counters = cat.GetCounters("");
+                rootnode.Nodes.Add(node);
+            }
+
+            // Add Nodes to TreeView
+            trvPerfs.Nodes.Add(rootnode);
+            trvPerfs.Nodes[0].SetIsCheckBoxVisible(false);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LoadPerformanceCounters(txtHostname.Text);
+        }
+
+        private void trvPerfs_AfterCheck(object sender, TreeViewEventArgs e)
+        {
+            if (e.Action != TreeViewAction.Unknown)
+            {
+                if (e.Node.Nodes.Count > 0)
+                {
+                    this.CheckAllChildNodes(e.Node, e.Node.Checked);
+                    this.SelectParents(e.Node, e.Node.Checked);
+                }
+            }
+        }
+
+        // Updates all child tree nodes recursively.
+        private void CheckAllChildNodes(TreeNode treeNode, bool nodeChecked)
+        {
+            foreach (TreeNode node in treeNode.Nodes)
+            {
+                node.Checked = nodeChecked;
+                if (node.Nodes.Count > 0)
+                {
+                    // If the current node has child nodes, call the CheckAllChildsNodes method recursively.
+                    this.CheckAllChildNodes(node, nodeChecked);
+                }
+            }
+        }
+
+        // Updates all parent tree nodes
+        private void SelectParents(TreeNode treeNode, bool isChecked)
+        {
+            var parent = treeNode.Parent;
+
+            if (parent == null)
+                return;
+            if (isChecked)
+            {
+                parent.Checked = true;
+                SelectParents(parent, true);
+            }
+            else
+            {
+                if (parent.Nodes.Cast<TreeNode>().Any(n => n.Checked))
+                    return; // do not uncheck parent if there are other checked nodes
+                SelectParents(parent, false);
+            }
+        }
+
+    }
+}
